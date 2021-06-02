ef9312ce8e27c9f9a367cf108b8264536e9526e7
hansenjl <jenn.leigh.hansen@gmail.com>
2021-06-02 08:44:09 -0700

    Create containers on the html

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log('hi')
+//Event Listener 1 - they hit a button and it fetches from the chuck norris API 
+
+
+// Display joke on screen and include a heart button for them to save it
+
+// Event Listener 2 - User can click the heart button and the joke will save down below. 
+
+// Event Listener 3 - Delete option for saved jokes 
+
+// stretch goal - add a db.json server to save the favorited jokes 
